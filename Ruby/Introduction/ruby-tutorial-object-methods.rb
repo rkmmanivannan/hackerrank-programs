ff0409272e74f7aff308ed065f3f1c8c6305bd6a
rkmmanivannan <rkm.manivannan@gmail.com>
2015-12-08 19:01:40 +0530

    my hackerrank practice program

---
+++ Ruby/Introduction/ruby-tutorial-object-methods.rb
@@ -0,0 +1,17 @@
+# Problem Statement
+#
+# Each object in Ruby may have methods associated with it. To demonstrate this, we want you to print whether a number is even or odd. A number has an even? method associated with it, which returns true or false based on the parity of the number.
+#
+# Assuming a variable number is already defined, check whether a given number is even or not.
+#
+# Hint
+#
+# Type in the code-editor
+#
+# return number.even?
+# or
+#
+# number.even?
+
+# add your code here
+number.even?
